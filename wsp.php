9a872c5e5b59fd79c9169e03afb6c53c9532a7bb
Budi Haryono <mail.budiharyono@gmail.com>
2024-04-29 05:47:40 +0700

    add cf

--- wsp.php
+++ wsp.php
@@ -17,4 +17,27 @@ define( 'WSP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 // Define Plugin URL.
 define( 'WSP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
+
+/**
+ * Check CF Loaded
+ */
+function wsp_call_carbon_fields() {
+	if ( ! class_exists( '\Carbon_Fields\Carbon_Fields' ) ) {
+		require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+		\Carbon_Fields\Carbon_Fields::boot();
+	}
+}
+
+/**
+ * MCS CF Loaded
+ */
+function wsp_cf_loaded() {
+	if ( ! function_exists( 'carbon_fields_boot_plugin' ) ) {
+		wsp_call_carbon_fields();
+	}
+}
+add_action( 'plugins_loaded', 'wsp_cf_loaded' );
+
+
+
 require_once WSP_PLUGIN_PATH . 'assets/assets.php';
